15e51fa1868bed4b1ec76f93777c3f0cde0ee0c4
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2024-07-26 18:47:53 -0400

    fix: sprite boss e elevator

--- Assets/Scripts/Tutorial/Elevator.cs
+++ Assets/Scripts/Tutorial/Elevator.cs
@@ -6,6 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class Elevator : MonoBehaviour
 {
+    private bool _entered;
+
+    private void Update()
+    {
+        if (_entered && Keyboard.current.fKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out PlayerController player))
+        {
+            _entered = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out PlayerController player))
+        {
+            _entered = false;
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.TryGetComponent(out PlayerController player))
